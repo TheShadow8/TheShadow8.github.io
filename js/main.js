7c6d9a51d12cc09fafb0a372faf731154a3cfaea
TheShadow8 <minhdo.ca@gmail.com>
2019-01-13 15:34:47 -0500

    next-prev gallery

--- js/main.js
+++ js/main.js
@@ -1,55 +1,40 @@
 // Close navbar when seclected nav item
 const navMenu = document.querySelector('.navigation__checkbox');
 const navList = document.querySelector('.navigation__list');
+const slider = document.querySelector('.items');
+const galleryPrev = document.getElementById('galleryPrev');
+const galleryNext = document.getElementById('galleryNext');
 
-navList.onclick = e => {
-  navMenu.checked = false;
+const scrollWidth = slider.scrollWidth;
+const offsetWidth = slider.offsetWidth;
+
+galleryPrev.onclick = e => {
+  e.preventDefault();
+  slider.scrollLeft = slider.scrollLeft - 300;
 };
 
-// Horizontal Click Drag Scroll Gallery
-const slider = document.querySelector('.items');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-// Scroll
-slider.addEventListener('wheel', e => {
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-
+galleryNext.onclick = e => {
   e.preventDefault();
-  const x = e.deltaY - slider.offsetLeft;
+  slider.scrollLeft = slider.scrollLeft + 300;
+};
 
-  const walk = (x - startX) / 2;
-
-  if (e.deltaY < 0) {
-    slider.scrollLeft = scrollLeft - walk;
-  } else {
-    slider.scrollLeft = scrollLeft + walk;
+const hiddenBtn = () => {
+  if (slider.scrollLeft < 10) {
+    galleryPrev.classList.add('hidden');
   }
+  if (slider.scrollLeft > 10) {
+    galleryPrev.classList.remove('hidden');
+  }
+
+  if (scrollWidth - 100 < slider.scrollLeft + offsetWidth) {
+    galleryNext.classList.add('hidden');
+  }
+
+  if (scrollWidth - 100 > slider.scrollLeft + offsetWidth) {
+    galleryNext.classList.remove('hidden');
+  }
+};
+
+slider.addEventListener('scroll', e => {
+  hiddenBtn();
 });
-
-// Click Drag
-// slider.addEventListener('mousedown', e => {
-//   isDown = true;
-//   slider.classList.add('active');
-//   startX = e.pageX - slider.offsetLeft;
-//   scrollLeft = slider.scrollLeft;
-// });
-// slider.addEventListener('mouseleave', () => {
-//   isDown = false;
-//   slider.classList.remove('active');
-// });
-// slider.addEventListener('mouseup', () => {
-//   isDown = false;
-//   slider.classList.remove('active');
-// });
-
-// slider.addEventListener('mousemove', e => {
-//   if (!isDown) return;
-//   e.preventDefault();
-//   const x = e.pageX - slider.offsetLeft;
-//   const walk = (x - startX) * 3; //scroll-fast
-//   slider.scrollLeft = scrollLeft - walk;
-// });
